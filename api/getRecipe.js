261e0da96e376933cb0273ad01279e9c85bb472c
J Hubert <jhprojectsnz@gmail.com>
2023-10-02 12:24:24 +1300

    Function regex debugging

--- api/getRecipe.js
+++ api/getRecipe.js
@@ -1,6 +1,7 @@
 export default async function handler(request, response) {
   const { extension = "" } = request.query;
   const regex = /^query=[\w ]+(&(maxReadyTime|intolerances|diet)=[\w ]+)*$/gi;
+  console.log(regex.test(extension));
 
   if (!regex.test(extension) || extension === "") {
     return response.status(400).end("Invalid input");
@@ -21,27 +22,3 @@ export default async function handler(request, response) {
     return response.status(502).end("Request Failed");
   }
 }
-
-// export default async function handler(request, response) {
-//     const { extension = "" } = request.query;
-//     const regex = /^query=[\w ]+(&(maxReadyTime|intolerances|diet)=[\w ]+)*$/gi;
-
-//     if (!regex.test(extension) || extension === "") {
-//       return response.status(400).end("Invalid input");
-//     }
-//     const key = process.env.API_KEY;
-//     try {
-//       const fetchResponse = await fetch(
-//         `https:URL-here?apiKey=${key}&${extension}`,
-//       );
-//       if (!fetchResponse.ok) {
-//         throw new Error(`HTTP error! status ${fetchResponse.status}`);
-//       }
-//       const recipeData = await fetchResponse.json();
-//       const newRecipe = recipeData.results[0];
-//       return response.status(200).json({ newRecipe });
-//     } catch (error) {
-//       console.error(error);
-//       return response.status(502).end("Request Failed");
-//     }
-//   }
